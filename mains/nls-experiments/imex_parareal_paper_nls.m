7d3b8fcea31b2f17bec7b3521f1e4a0fe49f32b6
buvoli <buvoli@uw.edu>
2020-09-16 17:33:49 -0700

    updated imex nls experiment

--- mains/nls-experiments/imex_parareal_paper_nls.m
+++ mains/nls-experiments/imex_parareal_paper_nls.m
@@ -12,14 +12,14 @@ equation_name = 'nls';
     cd(cwd)
 
 %% -- Reference Solution -----------------------------------------------------------------------------------------------
-erk_options = struct('coeffGenerator', @ERK4, 'parameters', pars,'max_ts_to_store', 2);
-Nt_ref = 75000;
-[TS, YS, tcpu_erkG, tccpu_erkG] = ERK(LF, NF, tspan, y0, Nt_ref, erk_options); 
+erk_options = struct('coeffGenerator', @IMRK4, 'parameters', pars,'max_ts_to_store', 2);
+Nt_ref = 2^19;
+[TS, YS, tcpu_erkG, tccpu_erkG] = IMRK(LF, NF, tspan, y0, Nt_ref, erk_options); 
 y_ref = YS(:, end);
     
 %% -- Experiment 1: Vary Nt, Fix Nt ------------------------------------------------------------------------------------ 
-steps    = 2 .^ (6:14);
-ks       = 0:3;
+steps    = 2 .^ (6:18);
+ks       = 0:6;
 Nts      = [512 1024 2048 4096 8192];
 num_fine = 16;
 
@@ -82,6 +82,15 @@ save(fullfile('data', [basename, '.mat']))
 
 %% -- generate plots ---------------------------------------------------------------------------------------------------
 hs = diff(tspan) ./ steps;
+scale_times = true;
+
+if(scale_times) % scale times relative to first non NaN fine rk
+    [~, ind] = find(~isnan([results_rk.error]), 1);
+    tsf = results_rk(ind).time; 
+else
+    tsf = 1;
+end
+
 legend_cell = [ {'IMEX-RK4'}, arrayfun(@(k) ['IMEX-PR (k=', num2str(k), ')'], ks, 'UniformOutput', false) ];
 rk_errors = [results_rk.error];
 rk_times = [results_rk.time];
@@ -101,23 +110,56 @@ for i = 1 : length(Nts)
         legend(legend_cell, 'Location', 'southeast'); legend box off;
     end
     axis([min(hs) max(hs) 1e-9 1e1]);
-    xlabel('stepsize $h$', 'interpreter', 'latex'); ylabel('relative error', 'interpreter', 'latex');
+    xlabel('stepsize $\Delta t$', 'interpreter', 'latex'); ylabel('relative error', 'interpreter', 'latex');
     title(title_str, 'interpreter', 'Latex');
     set(gca, 'FontSize', 15, 'FontName', 'Minion Pro');
     exportFigure(fh, struct('SavePath', fullfile('figures', basename), 'Format', 'jpg', 'PaperPosition', [0 0 12, 14]))
     
     % -- efficiency plots ----------------------------------------------------------------------------------------------
     basename = ['imex-nls-efficiency-NT', num2str(Nts(i))];
-    loglog(rk_times, rk_errors, '.-k', 'linewidth', 3, 'MarkerSize', 30); hold on;
-    loglog(p_times_matrix, p_error_matrix, '--', 'linewidth', 3, 'MarkerSize', 30); hold off;
-    legend(legend_cell, 'Location', 'northeast'); legend box off;
-    axis([min(rk_times) max(rk_times) 1e-9 1e1]);
-    xlabel('time (sec)'); ylabel('relative error');
+    loglog(rk_times / tsf, rk_errors, '.-k', 'linewidth', 3, 'MarkerSize', 30); hold on;
+    loglog(p_times_matrix / tsf, p_error_matrix, '--', 'linewidth', 3, 'MarkerSize', 30); hold off;
+    legend(legend_cell, 'Location', 'northeast'); %legend box off;
+    if(scale_times)
+        xlabel('relative time');
+        axis([1 max(rk_times / tsf) 1e-9 1e1]);
+    else
+        xlabel('time (sec)');
+        axis([min(rk_times) max(rk_times)/sf 1e-9 1e1]);
+    end
+    ylabel('relative error');    
     title(title_str, 'interpreter', 'Latex');
     set(gca, 'FontSize', 15, 'FontName', 'Minion Pro');
-    exportFigure(fh, struct('SavePath', fullfile('figures', basename), 'Format', 'jpg', 'PaperPosition', [0 0 12, 14]))
+    exportFigure(fh, struct('SavePath', fullfile('figures', basename), 'Format', 'jpg', 'PaperPosition', [0 0 14, 14]))
 
 end   
+
+%% -- generate adaptive efficiency diagrams ----------------------------------------------------------------------------
+rk_errors = [results_rk.error];
+rk_times  = [results_rk.time] / results_rk(2).time;
+
+% -- Nt = 2048, k=3 ----------------------------------------------------------------------------------------------------
+p_k3_error = [results_parareal(3, :, 4).error];
+p_k3_times = rk_times / 16.176935229067933;
+
+% -- Nt = 2048, adaptive k<=3 ------------------------------------------------------------------------------------------
+pfasst_data = csvread('pfasst-ka-data.csv'); % column1: steps, column2: error, column3: speedup
+p_ka_error = pfasst_data(:, 2);
+p_ka_times = rk_times(:) ./ pfasst_data(:, 3);
+
+fh = figure(1); clf;
+loglog(rk_times, rk_errors, '.-k', 'linewidth', 3, 'MarkerSize', 30); hold on;
+loglog(p_ka_times, p_ka_error, 's-.', 'color', [0.635000000000000   0.078000000000000   0.184000000000000], 'MarkerFaceColor', [0.635000000000000   0.078000000000000   0.184000000000000], 'linewidth', 3, 'MarkerSize', 13);
+loglog(p_k3_times, p_k3_error, '.--', 'color', [0.466000000000000   0.674000000000000   0.188000000000000], 'linewidth', 3, 'MarkerSize', 30);
+title_str = sprintf('{\\boldmath $N_T=%i$, $N_F=%i$, $N_P=%i$}', Nts(3), num_fine, Nts(3) / num_fine);
+title(title_str, 'interpreter', 'Latex');
+legend({'IMEX-RK4', 'IMEX-PR ($k\le3$)', 'IMEX-PR ($k=3$)'}, 'interpreter', 'latex'); legend box off;
+set(gca, 'FontSize', 15, 'FontName', 'Minion Pro');
+xlabel('relative time'); 
+ylabel('relative error');    
+axis([1 rk_times(end) 1e-9 1e1])
+
+exportFigure(fh, struct('SavePath', fullfile('figures', 'imex-nls-efficiency-adaptive'), 'Format', 'jpg', 'PaperPosition', [0 0 14, 14]))
 
 %% -- Experiment 2: Fix Nt, vary Nf ------------------------------------------------------------------------------------ 
 Nt_fixed  = 512;
@@ -182,7 +224,7 @@ loglog(hs, rk_errors, '.-k', 'linewidth', 3, 'MarkerSize', 30); hold on;
 loglog(hs, p_error_matrix, '.-', 'linewidth', 3, 'MarkerSize', 30); hold off;
 legend(legend_cell, 'Location', 'southeast'); legend box off;
 axis([min(hs) max(hs) 1e-9 1e1]);
-xlabel('stepsize $h$', 'interpreter', 'latex'); ylabel('relative error', 'interpreter', 'latex');
+xlabel('stepsize $\Delta t$', 'interpreter', 'latex'); ylabel('relative error', 'interpreter', 'latex');
 title(title_str, 'interpreter', 'Latex');
 set(gca, 'FontSize', 15, 'FontName', 'Minion Pro');
 exportFigure(fh, struct('SavePath', fullfile('figures', basename), 'Format', 'jpg', 'PaperPosition', [0 0 12, 14]))
